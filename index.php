fd81d07d83f0206db14a0b64a7027f62c55e6261
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-06-11 12:11:07 +0800

    BASTAA

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php include('nav.php'); ?>
-Hello world!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <table>
+            <tr>
+                <th>Row</th>
+                <th>Book Title</th>
+                <th>Description</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>HEEHEE</td>
+                <td>Billie Jeans</td>
+            </tr>
+        </table>
+    </section>
 </body>
 </html>
+
