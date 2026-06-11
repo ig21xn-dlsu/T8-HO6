52db949d79b94b52a6e8fd9cb0c118e579d8eaab
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-06-11 12:00:51 +0800

    Basta

---
+++ nav.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Books</title>
+    
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <a id="books" href="index.php">
+                Books
+            </a>
+            <a id="add-Books" href="add.php">
+                Add
+            </a>
+        </ul>
+    </nav>
+</body>
+</html>
