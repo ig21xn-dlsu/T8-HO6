fd2aef3b19277952f10422eb7125cfa1fbd4e562
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-06-28 17:26:54 +0800

    Finished styling add.php - ian

--- add.php
+++ add.php
@@ -1,12 +1,18 @@
 <?php include('nav.php'); ?>
 
-<form action="submit.php" method="POST">
-    <label for="title">Book Title:</label><br>
-    <input type="text" id="title" name="title"><br>
-    <label for="description">Description:</label><br>
-    <input type="text" id="description" name="description"><br><br>
-    <input type="submit" value="Submit">
-</form>
+<div class="form-container">
+    <h2 style="text-align: center; margin-bottom: 20px;">Add a Book</h2>
+
+    <form action="submit.php" method="POST">
+        <label for="title">Book Title:</label>
+        <input type="text" id="title" name="title" required>
+
+        <label for="description">Description:</label>
+        <input type="text" id="description" name="description" required>
+
+        <input type="submit" value="Submit">
+    </form>
+</div>
 
 </body>
 
