af1b094e14366df385dae0d6b30cfd2a5d407105
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-06-28 16:38:24 +0800

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,25 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body>
 
+<body>
     <nav>
         <ul>
-            <a id="books" href="index.php">
-                Books
-            </a>
-            <a id="add-Books" href="add.php">
-                Add
-            </a>
+            <a id="books" href="index.php">Books</a>
+            <a id="add-Books" href="add.php">Add </a>
         </ul>
     </nav>
-</body>
-</html>
